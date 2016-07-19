bfabd4334437fba53fc1da676263ad7e863c1bff
Nathaniel Sun <nathanielove@gmail.com>
2016-07-19 20:20:16 +0800

    added downloader

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # English Words Pronunciation MP3 Audio Download
 
+## Getting Started
+
+* Download & install [Python 3](https://www.python.org/downloads/)
+* Install dependencies
+
+```bash
+$ pip install -r requirements.txt
+```
+
+## Download All MP3
+
+Just do it.
+
+```bash
+$ python3 download_all_mp3.py
+```
+
+This command will start a downloader with 50 threads by default.
+
+To specify how many threads you want to use in the downloader, just pass in a positive integer as parameter:
+
+```bash
+$ python3 download_all_mp3.py 10
+```
+
+This will start a downloader with 10 concurrent threads.
+
+The downloaded mp3 audio files will be stored in the `download/` directory
+
+## Use `data.json` in Other Applications
+
 The [data.json](data.json) file contains the URLs to the pronunciation mp3 audio files for over 24,000 English words.
 
 A segment of the `data.json` file looks like this:
@@ -21,6 +52,8 @@ A segment of the `data.json` file looks like this:
 Apparently, each key is an English word and the value is the URL to the MP3 audio files.
 
 The `data.json` file is only 1.8MB so you can load it in your memory and let it serve as a lookup table. This should have covered most unique words out there in English. If the word you are looking for is not in the list, you should probably look for some other sources. Ask me for help if you wanna make a crawler and you don't know how.
+
+## Acknowledgements
 
 The word list was downloaded from [Mieliestronk's](http://www.mieliestronk.com/wordlist.html). There are 56K+ words in the list but only 24K+ are recognized as unique words.
 
---
+++ download_all_mp3.py
@@ -0,0 +1,105 @@
+import json
+import math
+import os
+import sys
+import threading
+from threading import Thread
+
+import requests
+
+MP3_FILENAME_EXTENSION = '.mp3'
+DIR_PATH = 'download/'
+TOTAL_THREADS = 50
+DATA_FILE = 'data.json'
+
+
+def download_mp3(word, url, dir_path):
+    filename = os.path.join(dir_path, word + MP3_FILENAME_EXTENSION)
+    with open(filename, 'wb') as file:
+        file.write(requests.get(url).content)
+
+
+# split a dictionary into a list of dictionaries
+def split_dict_evenly(m_dict, segment_count):
+    if segment_count == 1:
+        return [m_dict]
+
+    segment_length = math.ceil(len(m_dict) / segment_count)
+    keys = list(m_dict.keys())
+    key_groups = [keys[segment_length * i: segment_length * (i + 1)] for i in range(segment_count)]
+    return [{key: m_dict[key] for key in group} for group in key_groups]
+
+
+# a single downloader thread
+class DownloadWorker(Thread):
+    # 'pairs' is a dictionary
+    def __init__(self, pk, pairs, dir_path, statistics):
+        Thread.__init__(self)
+        self.pk = pk
+        self.pairs = pairs
+        self.dir_path = dir_path
+        self.statistics = statistics
+
+    def run(self):
+        for word, url in self.pairs.items():
+            current = self.statistics.increase_current()
+            print('(' + str(current) + '/' + str(self.statistics.total) + ') ' + word)
+            download_mp3(word, url, self.dir_path)
+
+
+# provide a mutex on a shared integer representing current progress
+class Statistics:
+    # pass in the total number in the constructor
+    def __init__(self, total):
+        self.total = total
+        self.current = 0
+        self.total_lock = threading.Lock()
+        self.current_lock = threading.Lock()
+
+    # an atom operation to increase the current progress
+    def increase_current(self):
+        self.current_lock.acquire()
+        self.current += 1
+        value = self.current
+        self.current_lock.release()
+        return value
+
+    def decrease_total(self):
+        self.total_lock.acquire()
+        self.total -= 1
+        value = self.total
+        self.total_lock.release()
+        return value
+
+
+def main(total_threads):
+    # create directory
+    if not os.path.exists(DIR_PATH):
+        os.makedirs(DIR_PATH)
+
+    # load data into dictionary
+    with open(DATA_FILE, 'r') as file:
+        data = json.loads(file.read())
+
+    # split dictionary into a list of dictionaries, each for a thread
+    data_segments = split_dict_evenly(data, total_threads)
+
+    # initialize shared object statistics
+    statistics = Statistics(len(data))
+
+    # start downloader threads
+    for i in range(total_threads):
+        worker = DownloadWorker(i + 1, data_segments[i], DIR_PATH, statistics)
+        worker.start()
+
+
+if __name__ == '__main__':
+    argument = TOTAL_THREADS
+
+    if len(sys.argv) == 2:
+        argument = int(sys.argv[1])
+
+    if argument > TOTAL_THREADS:
+        argument = TOTAL_THREADS
+    
+    main(argument)
